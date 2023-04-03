61a84b00c80f3a87eb5675e00332b0f4cf1a6f40
Toumi Mohamed Koussay <toumikoussay@hotmail.fr>
2023-04-03 13:27:26 +0100

    Adding arabic language support

--- app/src/main/java/com/example/myguide/MainActivity.java
+++ app/src/main/java/com/example/myguide/MainActivity.java
@@ -634,9 +634,10 @@ public class MainActivity extends AppCompatActivity {
                     Map<String, String> headers = new HashMap<>();
                     headers.put("X-RapidAPI-Host", "travel-advisor.p.rapidapi.com");
                     // headers.put("X-RapidAPI-Key", "9a8a6b8942msh7862e75a384b437p113bfajsn9368eb03cb21");
-                    headers.put("X-RapidAPI-Key", "016fa5cfbfmsh57370e6c5df1f30p1cdd22jsn969c05ef7a43");
-                  //  headers.put("X-RapidAPI-Key", "54da830830msh3b6cacf87fd75d3p196435jsn050cd5ee7973");
-
+                    //headers.put("X-RapidAPI-Key", "016fa5cfbfmsh57370e6c5df1f30p1cdd22jsn969c05ef7a43");
+                  //  headers.put("X-RapidAPI-Key", "54da830830msh3b6cacf87fd75d3p196435jsn050cd5ee7973")
+                    // ajad api key
+                    headers.put("X-RapidAPI-Key", "c3784205d8mshf89f1c53e38548ep161f68jsn59abcce9d967");
                     return headers;
                 }
             };
